c138e5212b3caa7ae0197e84843666757bbb3ab4
Shubham Karala <shubhamkarala@gmail.com>
2019-01-05 00:02:17 +0530

    Updates

---
+++ precache-manifest.b73939fad351919a2142fc951e9eacb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "f8bfef8712e8622f728d",
+    "url": "/todo-react/static/js/main.f8bfef87.chunk.js"
+  },
+  {
+    "revision": "6f96e78ba8e11d79daff",
+    "url": "/todo-react/static/js/1.6f96e78b.chunk.js"
+  },
+  {
+    "revision": "f8bfef8712e8622f728d",
+    "url": "/todo-react/static/css/main.b41a7975.chunk.css"
+  },
+  {
+    "revision": "b68474fcec4caf66c1998ea289ee3b35",
+    "url": "/todo-react/index.html"
+  }
+];
